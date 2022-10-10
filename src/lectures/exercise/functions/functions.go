4f3c4297140f66e4ed7e9fd54b1312d16d9f773e
Andrea Lin <andrealin03@gmail.com>
2022-10-10 21:33:15 +0100

    completed function exercise

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,6 +19,44 @@ package main
 
 import "fmt"
 
-func main() {
+//* Write a function that returns any number
+func anyNumber(number int) int {
+	return number
+}
+//* Write a function that returns any two numbers
+func anyTwoNumbers()(int, int) {
+	return 2,4
+}
 
+//* Add three numbers together using any combination of the existing functions.
+//* Write a function to add 3 numbers together, supplied as arguments, and
+
+func add(lhs, mid, rhs int) int {
+	return lhs + mid + rhs
+}
+
+//* Write a function that accepts a person's name as a function
+//  parameter and displays a greeting to that person.
+func greeting(name string){
+	 fmt.Println("Hello, ", name)
+}
+
+//* Write a function that returns any message, and call it from within
+//  fmt.Println()
+func echo()string{
+	return "Hi, there!"
+}
+
+func main() {
+	greeting("momo")
+	fmt.Println(echo())
+
+	fmt.Println("The answer is: ", add(1,2,3))
+
+	first, second := anyTwoNumbers()
+	fmt.Println("Return any two numbers, ", first, " and ", second)
+//* Add three numbers together using any combination of the existing functions.
+//  * Print the result
+
+	fmt.Println("Add three numbers together, ",add(first, second, 3))
 }
