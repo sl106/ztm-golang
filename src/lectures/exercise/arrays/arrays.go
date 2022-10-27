80a34860003b48b64ee09dfde1800e23606203a2
andrealin <andrealin03@gmail.com>
2022-10-27 20:59:41 +0100

    completed arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,39 @@ package main
 
 import "fmt"
 
+//* Using an array, create a shopping list with enough room
+//  for 4 products
+//  - Products must include the price and the name
+type product struct {
+	name string
+	price int
+}
+
+func printInfo (p [4]product) {
+	var	total, totalItem int
+	for _, product := range p {
+		total += product.price	
+		if product.name != "" {
+			totalItem += 1
+		}
+	}
+	fmt.Println("total cost of the items is: ", total)
+	fmt.Println("total number of items is: ", totalItem)
+	fmt.Println("the last item is: ",p[totalItem - 1])
+}
 func main() {
+  shoppingList := [4]product {
+	 {"milky", 1},
+	 {"soap", 2},
+	 {"water", 2},
+}
+printInfo(shoppingList)
+shoppingList[3] = product{"beef", 10}
+printInfo(shoppingList)
+//  - The last item on the list
+//  - The total number of items
+//  - The total cost of the items
+//* Add a fourth product to the list and print out the
+//  information again
 
 }
